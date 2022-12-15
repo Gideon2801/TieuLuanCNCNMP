ff83c0e2f2430198c9fd0c774d73f66371e9e124
linhtran-0209 <tranthikhanhlinh02092001@gmail.com>
2022-12-15 17:46:53 +0700

    Update frontend

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,14 +1,15 @@
 // Creating token and saving in cookie
 const sendToken = (user, statusCode, res, msg) => {
   const token = user.getJWTToken();
-
+  
   // options for cookies
   const options = {
-    expires: new Date(
-      Date.now + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
     httpOnly: true,
+    secure: false,
+    path: "/",
+    sameSite: "strict"
   };
+  
 
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
